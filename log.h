2160385b6819dc2d10358c152d15c56139af2d30
Jinglei Ren <jinglei.ren@gmail.com>
2013-02-25 17:39:46 +0800

    Add segment/log new/add/free operations.

--- log.h
+++ log.h
@@ -9,6 +9,8 @@
 #ifndef SESTET_RFFS_LOG_H_
 #define SESTET_RFFS_LOG_H_
 
+#include "sys.h"
+
 #define SEG_LEN 10240 // 10k
 #define CHUNK_SIZE 4096 // 512 bytes
 
@@ -23,26 +25,80 @@ struct segment {
   struct seg_entry entries[SEG_LEN];
   struct segment *prev;
   struct segment *next;
+  unsigned int seq_num;
+  spinlock_t lock;
 };
+
+static inline struct segment *seg_new() {
+  struct segment *seg = (struct segment *)MALLOC(sizeof(struct segment));
+  seg->prev = seg->next = 0;
+  spin_lock_init(&seg->lock);
+}
 
 struct log_pos { // only for internal use
   struct segment *seg;
   unsigned int entry;
 };
 
-struct log {
+struct rffs_log {
   struct log_pos log_begin;
   struct log_pos log_end;
   unsigned int length;
 };
 
-struct log *log_new();
+static inline struct rffs_log *log_new() {
+  struct segment *seg = seg_new();
+  struct rffs_log *log = (struct rffs_log *)MALLOC(sizeof(struct rffs_log));
 
-void log_del(struct log *log);
+  seg->seq_num = 0;
+  seg->prev = seg->next = seg;
 
-void log_append(struct log *log, struct seg_entry *entry);
+  log->log_begin.seg = seg;
+  log->log_begin.entry = SEG_LEN;
+  log->log_end.seg = seg;
+  log->log_end.entry = 0;
 
-void log_seal(struct log *log,
+  return log;
+}
+
+static inline void log_add_seg(struct rffs_log *log) {
+  struct segment *new_seg = seg_new();
+  struct segment *prev_seg = log->log_begin.seg->prev;
+  struct segment *next_seg = prev_seg->next;
+
+  spin_lock(&prev_seg->lock);
+  if (prev_seg != next_seg) {
+    spin_lock(&next_seg->lock);
+  }
+
+  prev_seg->next = new_seg;
+  new_seg->prev = prev_seg;
+  next_seg->prev = new_seg;
+  new_seg->next = next_seg;
+
+  spin_unlock(&prev_seg->lock);
+  if (prev_seg != next_seg) {
+    spin_unlock(&next_seg->lock);
+  }
+}
+
+static inline void log_free(struct rffs_log *log) {
+  struct segment *begin = log->log_begin.seg;
+  struct segment *seg = begin;
+  while (seg->next != begin) {
+    seg = seg->next;
+    MFREE(seg->prev);
+  }
+  MFREE(seg);
+
+  MFREE(log);
+}
+
+static inline void log_append(struct rffs_log *log, struct seg_entry *entry) {
+
+}
+
+void log_seal(struct rffs_log *log,
               struct log_pos *trans_begin, // output
               struct log_pos *trans_end); // output
 
