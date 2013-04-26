9f72698207958ac8c5a0c8075b59439a8119b66f
Jinglei Ren <jinglei.ren@gmail.com>
2013-04-26 11:00:07 +0800

    Cancel support for MacOS and Windows.

--- sys.h
+++ sys.h
@@ -19,29 +19,16 @@
   #define MFREE(p) free(p)
 #endif
 
-#ifdef _MSC_VER // for Microsoft compiler
-  #define inline __inline
-#endif
-
 #ifdef __KERNEL__
   #include <linux/spinlock.h>
   #define spin_lock_destroy(lock)
 #else
   #include <pthread.h>
-#if defined(__APPLE__) && defined(__MACH__)
-  #include <libkern/OSAtomic.h>
-  typedef OSSpinLock spinlock_t;
-  #define spin_lock_init(lock) (*lock = OS_SPINLOCK_INIT)
-  #define spin_lock(lock) OSSpinLockLock(lock)
-  #define spin_unlock(lock) OSSpinLockUnlock(lock)
-  #define spin_lock_destroy(lock)
-#else
   typedef pthread_spinlock_t spinlock_t;
   #define spin_lock_init(lock) pthread_spin_init(lock, PTHREAD_PROCESS_PRIVATE)
   #define spin_lock(lock) pthread_spin_lock(lock)
   #define spin_unlock(lock) pthread_spin_unlock(lock)
   #define spin_lock_destroy(lock) pthread_spin_destroy(lock)
-#endif // Mac OS
 #endif // Linux kernel
 
 #ifdef __KERNEL__
