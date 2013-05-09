3c155a1f3064eae6d7463925917623d6eb0c62c7
Jinglei Ren <jinglei.ren@gmail.com>
2013-05-09 11:21:20 +0800

    Clear header inclusions.

--- inode.c
+++ inode.c
@@ -11,17 +11,8 @@
 
 #include <linux/module.h>
 #include <linux/fs.h>
-#include <linux/pagemap.h>
-#include <linux/highmem.h>
-#include <linux/time.h>
-#include <linux/init.h>
-#include <linux/string.h>
 #include <linux/backing-dev.h>
-#include <linux/sched.h>
-#include <linux/parser.h>
-#include <linux/magic.h>
-#include <linux/slab.h>
-#include <asm/uaccess.h>
+#include <linux/pagemap.h>
 
 #include "hashtable.h"
 #include "rffs.h"
